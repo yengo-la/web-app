4073567a2d88991de9f3d729ce8b1cbb94020963
yengo-la <david.budzak91@gmail.com>
2025-05-29 00:35:43 +0200

    fancy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState} from 'react'
 import Weather from './Components/Weather.jsx'
 import Navbar from './Components/Navbar.jsx' 
 import NavbarExtension from './Components/Navbar-Extension.jsx'
@@ -6,29 +6,6 @@ import NavbarExtension from './Components/Navbar-Extension.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
-// precipitation,clouds, pressure, temperature and wind 41.0316005lat
-//21.3302511lon
-// app.get('/weather', (req, res) => {
-//   const query = `
-//     SELECT 
-//       city, 
-//       DATE_FORMAT(date, '%Y-%m-%d') AS date,
-//       TIME_FORMAT(time, '%H:%i') AS time,
-//       humidity,
-//       pressure,
-//       wind_speed,
-//       temperature,
-//       weather_type
-//     FROM weather_data
-//     ORDER BY date DESC, time DESC
-//     LIMIT 16
-//   `;
-
-//   db.query(query, (err, results) => {
-//     if (err) return res.status(500).json({ error: err.message });
-//     res.json(results);
-//   });
-// });
 
   return (
     <>
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -2,14 +2,25 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <div className="navbar">
-            
-            <a href="" className='navbarLogo'>
-                <h1>T-WEATHER</h1>
-            </a>
+        <>
+            <div className="intro-flash"></div>
 
-        </div>
+            <div className="navbar">
+                <a href="" className='navbarLogo'>
+                    <h1 className="glow-logo">T-WEATHER</h1>
+                </a>
+            </div>
+        </>
     )
 }
 
 export default Navbar
+
+
+        // <div className="navbar">
+            
+        //     <a href="" className='navbarLogo'>
+        //         <h1>T-WEATHER</h1>
+        //     </a>
+
+        // </div>
